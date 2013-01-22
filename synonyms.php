d28ac0bb48f738e9c6a7f31f1d7591ccaf2e21f6
Roderic Page <rdmpage@gmail.com>
2013-01-22 11:45:39 +0000

    Added some more examples

--- synonyms.php
+++ synonyms.php
@@ -4,7 +4,16 @@ require_once(dirname(__FILE__) . '/gnrd.php');
 
 // Grab text from BHL page, extract names, look for combinations that are likely to be synonyms
 
-$url = 'http://gnrd.globalnames.org/name_finder.json?url=http://biostor.org/bhlapi_page_text.php?PageID=2340880';
+$PageID = 2858128;
+
+$PageID = 40874129;
+
+$PageID = 2766281;
+
+// Check list of birds of the world
+$PageID = 14482542;
+
+$url = 'http://gnrd.globalnames.org/name_finder.json?url=http://biostor.org/bhlapi_page_text.php?PageID=' . $PageID;
 
 $response = get_names_from_url($url);
 
@@ -18,13 +27,15 @@ echo "Unique names\n";
 print_r($names);
 
 
-// Possible synonyms are names that are difefrent but have the last part the same, e.g.
+// Possible synonyms are names that are different but have the last part the same, e.g.
 // Hipposideros bicolor erigens
 // Hipposideros erigens
+// to do: specific epithet spelling may change with gender of genus (sigh)
 
 $synonyms = array();
 foreach ($names as $name)
 {
+	// species
 	if (preg_match('/^(?<genus>\w+)\s+(?<species>\w+)$/', $name, $m))
 	{
 		if (!isset($synonyms[$m['species']]))
@@ -33,6 +44,8 @@ foreach ($names as $name)
 		}
 		$synonyms[$m['species']][] = $m['genus'];
 	}
+	
+	// subspecies
 	if (preg_match('/^(?<genus>\w+)\s+(?<species>\w+)\s+(?<subspecies>\w+)$/', $name, $m))
 	{
 		if (!isset($synonyms[$m['subspecies']]))
@@ -41,6 +54,16 @@ foreach ($names as $name)
 		}
 		$synonyms[$m['subspecies']][] = $m['genus'] . ' ' . $m['species'];
 	}
+	
+	// subgenus
+	if (preg_match('/^(?<genus>\w+)\s+\((?<subgenus>\w+)\)\s+(?<species>\w+)$/', $name, $m))
+	{
+		if (!isset($synonyms[$m['species']]))
+		{
+			$synonyms[$m['species']] = array();
+		}
+		$synonyms[$m['species']][] = $m['genus'] . ' (' . $m['subgenus'] . ')';
+	}	
 	
 }
 
